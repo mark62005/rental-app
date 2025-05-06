234a0361a5c3dcd483e163508521d5d61b2ab9d9
Mark Wong <markwong.digital@gmail.com>
2025-05-06 14:24:19 -0700

    Build landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,18 @@
+import CTASection from "@/components/landing/sections/CTASection";
+import DiscoverSection from "@/components/landing/sections/DiscoverSection";
+import FeaturesSection from "@/components/landing/sections/FeaturesSection";
+import FooterSection from "@/components/landing/sections/FooterSection";
+import HeroSection from "@/components/landing/sections/HeroSection";
+
 function LandingPage() {
 	return (
-		<div>
-			<div className=""></div>LandingPage
-		</div>
+		<>
+			<HeroSection />
+			<FeaturesSection />
+			<DiscoverSection />
+			<CTASection />
+			<FooterSection />
+		</>
 	);
 }
 export default LandingPage;
