0347287f9a634d9a8b5759e63c636ec1cd003268
Mark Wong <markwong.digital@gmail.com>
2025-05-09 15:16:43 -0700

    Finish building API endpoint for getting auth user

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,14 @@
+import Navbar from "@/components/shared/Navbar";
+import LandingPage from "./(nondashboard)/landing/page";
+
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="h-full w-full">
+			<Navbar />
+
+			<main className="flex flex-col h-full w-full">
+				<LandingPage />
+			</main>
+		</div>
+	);
 }
