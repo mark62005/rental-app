064b3d01e27647bebcff6eca3b88b7d87715fda3
Mark Wong <markwong.digital@gmail.com>
2025-05-13 14:25:14 -0700

    Build settings page for updating tenant's profile

--- client/src/types/redux/api/auth.ts
+++ client/src/types/redux/api/auth.ts
@@ -1,8 +1,9 @@
 import { AuthUser } from "aws-amplify/auth";
-import { Manager, Tenant } from "@/types/prismaTypes";
+import { ITenant } from "@/types/app/tenants";
+import { Manager } from "@/types/prismaTypes";
 
 export interface IUser {
 	cognitoInfo: AuthUser;
-	userInfo: Tenant | Manager;
+	userInfo: ITenant | Manager;
 	userRole: string;
 }
