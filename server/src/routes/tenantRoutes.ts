9a65b92ee62e88ee6a62af3796434381fa48b9ad
Mark Wong <markwong.digital@gmail.com>
2025-05-20 16:50:25 -0700

    Create API endpoints for adding and removing property to a tenant's favorites

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
+	addFavoriteProperty,
 	createTenant,
 	getPropertiesByTenantId,
 	getTenant,
+	removeFavoriteProperty,
 	updateTenant,
 } from "../controllers/tenantControllers";
 
@@ -12,5 +14,7 @@ router.post("/", createTenant);
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.get("/:cognitoId/properties", getPropertiesByTenantId);
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
 
 export default router;
