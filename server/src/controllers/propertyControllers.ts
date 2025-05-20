a3ff0dbcdc41853b0fc16d5dd87ca7fb8fba6bca
Mark Wong <markwong.digital@gmail.com>
2025-05-20 10:15:52 -0700

    Create API endpoint for creating property

--- server/src/controllers/propertyControllers.ts
+++ server/src/controllers/propertyControllers.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
-import { Prisma, PrismaClient } from "../generated/prisma";
+import { Prisma, PrismaClient, Location } from "../generated/prisma";
 import { wktToGeoJSON } from "@terraformer/wkt";
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client } from "@aws-sdk/client-s3";
+import axios from "axios";
 
 const prisma = new PrismaClient();
+const s3Client = new S3Client({
+	region: process.env.AWS_REGION,
+});
 
 export async function getFilteredProperties(
 	req: Request,
@@ -188,5 +194,114 @@ export async function getPropertyWithId(
 	} catch (error) {
 		console.error("Error retrieving property: ", error);
 		res.status(500).json({ message: "Error retrieving property." });
+	}
+}
+
+export async function createProperty(
+	req: Request,
+	res: Response
+): Promise<void> {
+	console.log("/ POST called");
+
+	try {
+		const files = req.files as Express.Multer.File[];
+		const {
+			address,
+			city,
+			state,
+			country,
+			postalCode,
+			managerCognitoId,
+			...propertyData
+		} = req.body;
+
+		const photoUrls = await Promise.all(
+			files.map(async (file) => {
+				const uploadParams = {
+					Bucket: process.env.S3_BUCKET_NAME!,
+					Key: `properties/${Date.now()}-${file.originalname}`,
+					Body: file.buffer,
+					ContentType: file.mimetype,
+				};
+
+				const uploadResult = await new Upload({
+					client: s3Client,
+					params: uploadParams,
+				}).done();
+
+				return uploadResult.Location;
+			})
+		);
+
+		const geocodingUrl = `
+			https://nominatim.openstreetmap.org/search?${new URLSearchParams({
+				street: address,
+				city,
+				country,
+				postalcode: postalCode,
+				format: "json",
+				limit: "1",
+			}).toString()}
+		`;
+
+		const geocodingResponse = await axios.get(geocodingUrl, {
+			headers: {
+				"User-Agent": "RealEstateApp (justsomedummyemail@gmail.com",
+			},
+		});
+
+		const [longitude, latitude] =
+			geocodingResponse.data[0]?.lon && geocodingResponse.data[0]?.lat
+				? [
+						parseFloat(geocodingResponse.data[0]?.lon),
+						parseFloat(geocodingResponse.data[0]?.lat),
+					]
+				: [0, 0];
+
+		// create location
+		const [location] = await prisma.$queryRaw<Location[]>`
+      INSERT INTO "Location" (address, city, state, country, "postalCode", coordinates)
+      VALUES (${address}, ${city}, ${state}, ${country}, ${postalCode}, ST_SetSRID(ST_MakePoint(${longitude}, ${latitude}), 4326))
+      RETURNING id, address, city, state, country, "postalCode", ST_AsText(coordinates) as coordinates;
+    `;
+
+		const amenities =
+			typeof propertyData.amenities === "string"
+				? propertyData.amenities.split(",")
+				: [];
+		const highlights =
+			typeof propertyData.highlights === "string"
+				? propertyData.highlights.split(",")
+				: [];
+
+		// Create property
+		const newProperty = await prisma.property.create({
+			data: {
+				...propertyData,
+				photoUrls,
+				locationId: location.id,
+				managerCognitoId,
+				amenities,
+				highlights,
+				isPetsAllowed: propertyData.isPetsAllowed === "true",
+				isParkingIncluded: propertyData.isParkingIncluded === "true",
+				pricePerMonth: parseFloat(propertyData.pricePerMonth),
+				securityDeposit: parseFloat(propertyData.securityDeposit),
+				applicationFee: parseFloat(propertyData.applicationFee),
+				beds: parseInt(propertyData.beds),
+				baths: parseFloat(propertyData.baths),
+				squareFeet: parseInt(propertyData.squareFeet),
+			},
+			include: {
+				location: true,
+				manager: true,
+			},
+		});
+
+		console.log(`Created property id: ${newProperty.id} successfully.`);
+		res.status(201).json({ data: newProperty });
+	} catch (error) {
+		console.error("Error creating property: ", error);
+		res.status(500).json({ message: "Error creating property." });
 	}
 }
--- server/src/routes/propertyRoutes.ts
+++ server/src/routes/propertyRoutes.ts
@@ -1,12 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 import {
+	createProperty,
 	getFilteredProperties,
 	getPropertyWithId,
 } from "../controllers/propertyControllers";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 router.get("/", getFilteredProperties);
+router.post(
+	"/",
+	authMiddleware(["manager"]),
+	upload.array("photos"),
+	createProperty
+);
 router.get("/:id", getPropertyWithId);
 
 export default router;
