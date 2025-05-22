f865f217c34119d26f615250b8e96913d4ae1559
Mark Wong <markwong.digital@gmail.com>
2025-05-22 16:56:49 -0700

    Create API endpoints for Application model

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,7 @@ import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
 import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -29,6 +30,7 @@ app.get("/test", (req, res) => {
 });
 app.use("/properties", propertyRoutes);
 app.use("/leases", leaseRoutes);
+app.use("/applications", applicationRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
